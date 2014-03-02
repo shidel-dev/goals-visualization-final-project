ef3013e7ceb6d3b9f1ff1fef1a41653e380add3a
Lorenanicole <me@lorenamesa.com>
2014-03-02 13:57:43 -0600

    added partial navbar for sign in and signout functionality, corrected signout routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
 
     if user && user.authenticate(params[:session][:password])
-      debugger
+      # debugger
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
