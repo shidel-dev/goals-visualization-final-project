1fac499313bc5d1adff238e6271b7a27b739a7f2
Lorenanicole <me@lorenamesa.com>
2014-03-02 16:39:23 -0600

    added validations on user model for email format, email uniquness, password length. added placeholders on user creation form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
   has_secure_password
 
   before_create :create_life_id
