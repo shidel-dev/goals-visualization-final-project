fc5f2753de3fff91eb12f958ad03d1811c2c0084
Lorenanicole <me@lorenamesa.com>
2014-03-04 11:13:53 -0600

    added /goals route for user to see goals with d3 word cloud, added #reflection_words and #goal_words on user model to find text of reflections and goal titles

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,17 @@ class User < ActiveRecord::Base
     @life = Life.create
     self.life_id = @life.id.to_s
   end
+
+  def reflection_words
+    @life_goals = Life.where(id: self.life_id).to_a
+    @life_goals = @life_goals[0]["nodes"]
+    arr = @life_goals.collect { |x| x["reflection"] if !x["reflection"].blank? }.compact
+  end
+
+  def goal_words
+    @life_goals = Life.where(id: self.life_id).to_a
+    @life_goals = @life_goals[0]["nodes"]
+    arr = @life_goals.collect { |x| x["title"] }.reject {|x| x == "\n    "}.compact
+  end
+
 end
