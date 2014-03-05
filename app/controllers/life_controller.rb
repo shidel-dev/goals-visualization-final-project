1246214fc5e51e43d1762e5e7f29a589d8f8d2d7
Brian Murphy <bmurphy1@mac.com>
2014-03-04 19:22:54 -0600

    Fix template missing render error

--- app/controllers/life_controller.rb
+++ app/controllers/life_controller.rb
@@ -1,16 +1,16 @@
 class LifeController < ApplicationController
+  respond_to :json
 
   def save
     @life = Life.find(current_user.life_id)
     @life.update(params[:life_data])
     @life.save!
+    render nothing: true
   end
 
   def load
     @life = Life.find(current_user.life_id)
-    respond_to do |format|
-      format.json { render json: @life }
-    end
+    render json: @life
   end
 
   def permit_params
